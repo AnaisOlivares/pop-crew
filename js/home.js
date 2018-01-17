d9e1f529fda836ae5b7e97d51eb0b88657bcd4fa
Lauraisa <galindolaura.isabel@gmail.com>
2018-01-17 12:54:42 -0500

    detallando home

--- js/home.js
+++ js/home.js
@@ -2,4 +2,17 @@ $(document).ready(function()
 {
   $('#slider1').tinycarousel();
   $('.slider2').tinycarousel()
+
+  // $('#slick1').slick({
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  // });
+  // $('.slider2').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  // });
 });
