4fb8c118adb5da0f4f972998a8e708e54761f7fe
Lauraisa <galindolaura.isabel@gmail.com>
2018-01-18 12:52:43 -0500

    trabajando search

--- js/home.js
+++ js/home.js
@@ -15,4 +15,29 @@ $(document).ready(function()
   //   autoplay: true,
   //   autoplaySpeed: 2000,
   // });
+
+  var $menuBar = $('#inputGroups');
+  $('#inputGroups').on('input', function() {
+    if (sessionStorage.getItem('movieId')) {
+      $idMovie = sessionStorage.getItem('movieId');
+      $menubar.show(data);
+    }
+  var menuSearch =  $('.genero-movie');
+  $('.genero-movie').on('click', function() {
+  
+    $('#container-peliculas').append('h2');
+    $('h2').prependTo().menuSearch.attr('title');
+    $('#container-peliculas').show();
+
+
+    // $("a").on("click", function(event)
+    // {
+    //     //Cancela el evento del href, por defecto al pinchar en un link
+    //     // te envia a este, con esto evitamos que ejecute el link
+    //     event.preventDefault();            
+
+    //    //Cargamos el contenido del enlace
+    //     $('#contenido').load(this.href);
+  })
+ 
 });
